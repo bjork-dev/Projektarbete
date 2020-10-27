1adf11cc78e887c729fc3979972e4159d50b3a0b
Liam Björkman <liambjorkman00@outlook.com>
2020-10-27 11:06:06 +0100

    Init layout

--- Butik/MainWindow.xaml.cs
+++ Butik/MainWindow.xaml.cs
@@ -26,9 +26,9 @@ namespace Butik
         private void Start()
         {
             // Window options
-            Title = "GUI App";
-            Width = 400;
-            Height = 300;
+            Title = "Store";
+            Width = 1000;
+            Height = 600;
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
 
             // Scrolling
@@ -41,7 +41,112 @@ namespace Butik
             root.Content = grid;
             grid.Margin = new Thickness(5);
             grid.RowDefinitions.Add(new RowDefinition());
+            grid.RowDefinitions.Add(new RowDefinition());
+            grid.RowDefinitions.Add(new RowDefinition());
+            grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
             grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+
+            //Title text
+            TextBlock titleText = new TextBlock
+            {
+                Text = "Store",
+                TextWrapping = TextWrapping.Wrap,
+                Margin = new Thickness(5),
+                FontFamily = new FontFamily("Constantia"),
+                FontSize = 25,
+                TextAlignment = TextAlignment.Center
+            };
+            grid.Children.Add(titleText);
+            Grid.SetColumn(titleText, 0);
+            Grid.SetRow(titleText, 0);
+            Grid.SetColumnSpan(titleText, 2);
+
+            //to be changed
+
+            //Item 1 label
+            Label itemLabel = new Label
+            {
+                Content = "test1",
+                Margin = new Thickness(5),
+                FontFamily = new FontFamily("Constantia"),
+                FontSize = 25,
+                HorizontalContentAlignment = HorizontalAlignment.Center
+            };
+            grid.Children.Add(itemLabel);
+            Grid.SetColumn(itemLabel, 0);
+            Grid.SetRow(itemLabel, 1);
+
+            //Item 1 label
+            Label itemLabel2 = new Label
+            {
+                Content = "test2",
+                Margin = new Thickness(5),
+                FontFamily = new FontFamily("Constantia"),
+                FontSize = 25,
+                HorizontalContentAlignment = HorizontalAlignment.Center
+            };
+            grid.Children.Add(itemLabel2);
+            Grid.SetColumn(itemLabel2, 0);
+            Grid.SetRow(itemLabel2, 2);
+            Label itemLabel3 = new Label
+            {
+                Content = "test3",
+                Margin = new Thickness(5),
+                FontFamily = new FontFamily("Constantia"),
+                FontSize = 25,
+                HorizontalContentAlignment = HorizontalAlignment.Center
+
+            };
+            grid.Children.Add(itemLabel3);
+            Grid.SetColumn(itemLabel3, 1);
+            Grid.SetRow(itemLabel3, 1);
+
+            //Item 1 label
+            Label itemLabel4 = new Label
+            {
+                Content = "test4",
+                Margin = new Thickness(5),
+                FontFamily = new FontFamily("Constantia"),
+                FontSize = 25,
+                HorizontalContentAlignment = HorizontalAlignment.Center
+            };
+            grid.Children.Add(itemLabel4);
+            Grid.SetColumn(itemLabel4, 1);
+            Grid.SetRow(itemLabel4, 2);
+
+
+            //Cart text
+            TextBlock cartText = new TextBlock
+            {
+                Text = "Cart",
+                TextWrapping = TextWrapping.Wrap,
+                Margin = new Thickness(5),
+                FontFamily = new FontFamily("Constantia"),
+                FontSize = 25,
+                TextAlignment = TextAlignment.Center
+            };
+            grid.Children.Add(cartText);
+            Grid.SetColumn(cartText, 3);
+            Grid.SetRow(cartText, 0);
+            Grid.SetColumnSpan(cartText, 2);
+
+
+            //Save cart button
+            Button saveBtn = new Button
+            {
+                Content = "Save cart",
+                Margin = new Thickness(5),
+                Padding = new Thickness(5),
+                FontFamily = new FontFamily("Constantia"),
+                FontSize = 20
+            };
+            grid.Children.Add(saveBtn);
+            Grid.SetColumn(saveBtn, 0);
+            Grid.SetRow(saveBtn, 4);
         }
     }
 }
