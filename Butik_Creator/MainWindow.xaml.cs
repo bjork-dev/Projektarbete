0a06536563233f1804df64c08e5584d9040dce7a
Svetlana Ernyleva <70198472+SvetlanaErn@users.noreply.github.com>
2020-11-06 00:18:45 +0100

    New Layout for Discounts

--- Butik_Creator/MainWindow.xaml.cs
+++ Butik_Creator/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,7 @@ namespace Butik_Creator
 {
     public partial class MainWindow : Window
     {
+        string discountPath = @"C:\Windows\Temp\Coupon.csv";
         public MainWindow()
         {
             InitializeComponent();
@@ -45,8 +47,8 @@ namespace Butik_Creator
             grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
             grid.RowDefinitions.Add(new RowDefinition());
 
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(3, GridUnitType.Star) });
             grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(2, GridUnitType.Star) });
-            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
 
             //Title Assortment
             TextBlock assortmentHeading = new TextBlock
@@ -73,18 +75,18 @@ namespace Butik_Creator
             Grid.SetRow(discountHeading, 0);
 
             // Create Assortment Panel
-            Grid assortmentPanel = CreatAssortmentPanel();
+            Grid assortmentPanel = CreateAssortmentPanel();
             grid.Children.Add(assortmentPanel);
             Grid.SetRow(assortmentPanel, 1);
             Grid.SetColumn(assortmentPanel, 0);
 
             // Create Discount Panel
-            Grid discountPanel = CreatDiscountPanel();
+            Grid discountPanel = CreateDiscountPanel();
             grid.Children.Add(discountPanel);
             Grid.SetRow(discountPanel, 1);
             Grid.SetColumn(discountPanel, 1);
         }
-        private Grid CreatAssortmentPanel()
+        private Grid CreateAssortmentPanel()
         {
             // Nested grid for the part Assortment
             Grid assortmentGrid = new Grid
@@ -101,71 +103,177 @@ namespace Butik_Creator
             return assortmentGrid;
         }
 
-        private Grid CreatDiscountPanel()
+        private Grid CreateDiscountPanel()
         {
             // Nested grid for the part Discount
             Grid discountGrid = new Grid
             {
                 Margin = new Thickness(5)
             };
+            discountGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            discountGrid.ColumnDefinitions.Add(new ColumnDefinition());
             discountGrid.RowDefinitions.Add(new RowDefinition());
             discountGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
-            discountGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
 
-            // Table
-            DataGrid dataGrid = new DataGrid();
-            discountGrid.Children.Add(dataGrid);
-            Grid.SetRow(dataGrid, 0);
-
-            DataTable table = new DataTable();
-            table.Columns.Add(new DataColumn("Code", typeof(string)));
-            table.Columns.Add(new DataColumn("Discount, %", typeof(int)));
-            table.Columns.Add(new DataColumn("Remove", typeof(bool)));
-
-            table.Rows.Add(new object[] { "code1", 1, false });
-
-            dataGrid.ItemsSource = table.DefaultView;
-
-
-            // StackPanel 
-            StackPanel panelRemoveButton = new StackPanel { Orientation = Orientation.Horizontal, HorizontalAlignment = HorizontalAlignment.Right };
-            discountGrid.Children.Add(panelRemoveButton);
-            Grid.SetRow(panelRemoveButton, 1);
-
-            // CheckBox 
-            CheckBox markAllCheckBox = new CheckBox
+            ListBox discountListBox = new ListBox
             {
-                Content = "Select all",
-                FontSize = 15,
                 Margin = new Thickness(5),
-                HorizontalAlignment = HorizontalAlignment.Left,
-                VerticalAlignment = VerticalAlignment.Center
+                MaxHeight = 500
             };
-            panelRemoveButton.Children.Add(markAllCheckBox);
+            discountGrid.Children.Add(discountListBox);
+            Grid.SetColumn(discountListBox, 0);
+            Grid.SetRow(discountListBox, 0);
 
-            // Button Remove
+            discountListBox.Items.Add("code1   1%");
+            discountListBox.Items.Add("code5   5%");
+            discountListBox.Items.Add("12345678912345678912   100%");
+
+            // Nested grid for buttons Remove and RemoveAll
+            Grid removeButtonGrid = new Grid { Margin = new Thickness(5) };
+            removeButtonGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            removeButtonGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            discountGrid.Children.Add(removeButtonGrid);
+            Grid.SetRow(removeButtonGrid, 1);
+            Grid.SetColumn(removeButtonGrid, 0);
+
             Button removeButton = new Button
             {
-                Content = "Remove selected",
+                Content = "Remove",
                 Margin = new Thickness(5),
-                FontSize = 15,
-                HorizontalAlignment = HorizontalAlignment.Right,
+                FontSize = 12,
                 Padding = new Thickness(5)
             };
-            panelRemoveButton.Children.Add(removeButton);
+            removeButtonGrid.Children.Add(removeButton);
+            Grid.SetColumn(removeButton, 0);
 
-            // Button Save
+            Button removeAllButton = new Button
+            {
+                Content = "Remove all",
+                Margin = new Thickness(5),
+                FontSize = 12,
+                Padding = new Thickness(5)
+            };
+            removeButtonGrid.Children.Add(removeAllButton);
+            Grid.SetColumn(removeAllButton, 1);
+
+            // Nested grid for textBoxes Code, Discount, buttons SaveChanges, Discart, AddNew
+            Grid addDiscountGrid = new Grid { Margin = new Thickness(5) };
+            addDiscountGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            addDiscountGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            addDiscountGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+            addDiscountGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+            addDiscountGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+            addDiscountGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+            addDiscountGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+            discountGrid.Children.Add(addDiscountGrid);
+            Grid.SetRow(addDiscountGrid, 0);
+            Grid.SetColumn(addDiscountGrid, 1);
+
+            Label codeLabel = new Label
+            {
+                Content = "Code",
+                Margin = new Thickness(5),
+                HorizontalContentAlignment = HorizontalAlignment.Right
+            };
+            addDiscountGrid.Children.Add(codeLabel);
+            Grid.SetColumn(codeLabel, 0);
+            Grid.SetRow(codeLabel, 0);
+
+            TextBox codeTextBox = new TextBox
+            {
+                Margin = new Thickness(5),
+                VerticalAlignment = VerticalAlignment.Center,
+                MaxLength = 20
+            };
+            addDiscountGrid.Children.Add(codeTextBox);
+            Grid.SetColumn(codeTextBox, 1);
+            Grid.SetRow(codeTextBox, 0);
+
+            Label discountLabel = new Label
+            {
+                Content = "Discount",
+                Margin = new Thickness(5),
+                HorizontalContentAlignment = HorizontalAlignment.Right
+            };
+            addDiscountGrid.Children.Add(discountLabel);
+            Grid.SetColumn(discountLabel, 0);
+            Grid.SetRow(discountLabel, 1);
+
+            TextBox discountTextBox = new TextBox
+            {
+                Margin = new Thickness(5),
+                VerticalAlignment = VerticalAlignment.Center
+            };
+            addDiscountGrid.Children.Add(discountTextBox);
+            Grid.SetColumn(discountTextBox, 1);
+            Grid.SetRow(discountTextBox, 1);
+
+            Button addButton = new Button
+            {
+                Content = "Add new",
+                Margin = new Thickness(5),
+                FontSize = 12,
+                Padding = new Thickness(5),
+            };
+            Grid.SetRow(addButton, 2);
+            Grid.SetColumn(addButton, 0);
+            addDiscountGrid.Children.Add(addButton);
+            Grid.SetColumnSpan(addButton, 2);
+
+            Button saveChangesButton = new Button
+            {
+                Content = "Save changes",
+                Margin = new Thickness(5),
+                FontSize = 12,
+                Padding = new Thickness(5),
+                IsEnabled = false
+            };
+            Grid.SetRow(saveChangesButton, 3);
+            Grid.SetColumn(saveChangesButton, 0);
+            addDiscountGrid.Children.Add(saveChangesButton);
+
+            Button discartButton = new Button
+            {
+                Content = "Discart",
+                Margin = new Thickness(5),
+                FontSize = 12,
+                Padding = new Thickness(5),
+                IsEnabled = false
+            };
+            Grid.SetRow(discartButton, 3);
+            Grid.SetColumn(discartButton, 1);
+            addDiscountGrid.Children.Add(discartButton);
+
+            TextBlock discountInfo = new TextBlock
+            {
+                Text = "Select a discount code to change",
+                HorizontalAlignment = HorizontalAlignment.Center,
+                Margin = new Thickness(5),
+                FontSize = 12,
+            };
+            Grid.SetRow(discountInfo, 4);
+            Grid.SetColumn(discountInfo, 0);
+            addDiscountGrid.Children.Add(discountInfo);
+            Grid.SetColumnSpan(discountInfo, 2);
+
             Button saveButton = new Button
             {
-                Content = "Save",
-                Margin = new Thickness(5, 20, 5, 5),
-                FontSize = 15,
-                Padding = new Thickness(10),
+                Content = "Save to file",
+                Margin = new Thickness(10),
+                FontSize = 12,
+                Padding = new Thickness(5),
             };
-            Grid.SetRow(saveButton, 2);
+            Grid.SetRow(saveButton, 1);
+            Grid.SetColumn(saveButton, 1);
             discountGrid.Children.Add(saveButton);
+            saveButton.Click += SaveButton_Click;
 
             return discountGrid;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
