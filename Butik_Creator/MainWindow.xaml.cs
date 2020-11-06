1da53868acee7e768b1700442821b28c75e4e20e
Svetlana Ernyleva <70198472+SvetlanaErn@users.noreply.github.com>
2020-11-06 11:39:02 +0100

    Layout for Assortment

--- Butik_Creator/MainWindow.xaml.cs
+++ Butik_Creator/MainWindow.xaml.cs
@@ -95,9 +95,220 @@ namespace Butik_Creator
             };
             assortmentGrid.RowDefinitions.Add(new RowDefinition());
             assortmentGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
-            assortmentGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            assortmentGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+            assortmentGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+            assortmentGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            assortmentGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(3, GridUnitType.Star) });
 
+            ListBox assortmentListBox = new ListBox
+            {
+                Margin = new Thickness(5),
+                //   MaxHeight = 300
+            };
+            assortmentGrid.Children.Add(assortmentListBox);
+            Grid.SetColumn(assortmentListBox, 0);
+            Grid.SetRow(assortmentListBox, 0);
 
+            assortmentListBox.Items.Add("item 1  100 kr");
+            assortmentListBox.Items.Add("item 2  200 kr");
+            assortmentListBox.Items.Add("item 3  300 kr");
+
+            Button removeAllButton = new Button
+            {
+                Content = "Remove all",
+                Margin = new Thickness(5),
+                FontSize = 12,
+                Padding = new Thickness(5)
+            };
+            assortmentGrid.Children.Add(removeAllButton);
+            Grid.SetColumn(removeAllButton, 0);
+            Grid.SetRow(removeAllButton, 1);
+
+            Label galleryLabel = new Label
+            {
+                Content = "Bild gallery",
+                FontSize = 15,
+                Margin = new Thickness(5),
+                HorizontalContentAlignment = HorizontalAlignment.Center
+            };
+            assortmentGrid.Children.Add(galleryLabel);
+            Grid.SetColumn(galleryLabel, 0);
+            Grid.SetRow(galleryLabel, 2);
+            Grid.SetColumnSpan(galleryLabel, 2);
+
+            WrapPanel bildGallery = new WrapPanel { Orientation = Orientation.Horizontal };
+            assortmentGrid.Children.Add(bildGallery);
+            Grid.SetColumn(bildGallery, 0);
+            Grid.SetRow(bildGallery, 3);
+            Grid.SetColumnSpan(bildGallery, 2);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Image testImage = new Image
+                {
+                    Source = new BitmapImage(new Uri(@"Images\lundgrens.jpg", UriKind.Relative)),
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    Margin = new Thickness(5),
+                    Width = 80,
+                    Height = 80,
+                    Stretch = Stretch.Uniform
+                };
+                RenderOptions.SetBitmapScalingMode(testImage, BitmapScalingMode.HighQuality);
+                bildGallery.Children.Add(testImage);
+
+                Image testImage3 = new Image
+                {
+                    Source = new BitmapImage(new Uri(@"Images\siberia.png", UriKind.Relative)),
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    Margin = new Thickness(5),
+                    Width = 80,
+                    Height = 80,
+                    Stretch = Stretch.Uniform
+                };
+                RenderOptions.SetBitmapScalingMode(testImage3, BitmapScalingMode.HighQuality);
+                bildGallery.Children.Add(testImage3);
+            }
+
+            Grid nestedGrid = new Grid
+            {
+                Margin = new Thickness(5)
+            };
+
+            nestedGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+            nestedGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            nestedGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            nestedGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            nestedGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+            nestedGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+            nestedGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+            nestedGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+            nestedGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+            assortmentGrid.Children.Add(nestedGrid);
+            Grid.SetColumn(nestedGrid, 1);
+            Grid.SetRow(nestedGrid, 0);
+
+            Label nameLabel = new Label
+            {
+                Content = "Name",
+                Margin = new Thickness(5),
+                HorizontalContentAlignment = HorizontalAlignment.Right
+            };
+            nestedGrid.Children.Add(nameLabel);
+            Grid.SetColumn(nameLabel, 0);
+            Grid.SetRow(nameLabel, 0);
+
+            TextBox nameTextBox = new TextBox
+            {
+                Margin = new Thickness(5)
+            };
+            nestedGrid.Children.Add(nameTextBox);
+            Grid.SetColumn(nameTextBox, 1);
+            Grid.SetRow(nameTextBox, 0);
+            Grid.SetColumnSpan(nameTextBox, 2);
+
+            Label descriptionLabel = new Label
+            {
+                Content = "Description",
+                Margin = new Thickness(5),
+                HorizontalContentAlignment = HorizontalAlignment.Right
+            };
+            nestedGrid.Children.Add(descriptionLabel);
+            Grid.SetColumn(descriptionLabel, 0);
+            Grid.SetRow(descriptionLabel, 1);
+
+            TextBox descriptionTextBox = new TextBox
+            {
+                Margin = new Thickness(5)
+            };
+            nestedGrid.Children.Add(descriptionTextBox);
+            Grid.SetColumn(descriptionTextBox, 1);
+            Grid.SetRow(descriptionTextBox, 1);
+            Grid.SetColumnSpan(descriptionTextBox, 2);
+
+            Label priceLabel = new Label
+            {
+                Content = "Price",
+                Margin = new Thickness(5),
+                HorizontalContentAlignment = HorizontalAlignment.Right
+            };
+            nestedGrid.Children.Add(priceLabel);
+            Grid.SetColumn(priceLabel, 0);
+            Grid.SetRow(priceLabel, 2);
+
+            TextBox priceTextBox = new TextBox
+            {
+                Margin = new Thickness(5)
+            };
+            nestedGrid.Children.Add(priceTextBox);
+            Grid.SetColumn(priceTextBox, 1);
+            Grid.SetRow(priceTextBox, 2);
+            Grid.SetColumnSpan(priceTextBox, 2);
+
+            Image testImage2 = new Image
+            {
+                Source = new BitmapImage(new Uri(@"Images\lundgrens.jpg", UriKind.Relative)),
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Margin = new Thickness(5),
+                Width = 80,
+                Height = 80,
+                Stretch = Stretch.Uniform
+            };
+            RenderOptions.SetBitmapScalingMode(testImage2, BitmapScalingMode.HighQuality);
+            nestedGrid.Children.Add(testImage2);
+            Grid.SetColumn(testImage2, 3);
+            Grid.SetRow(testImage2, 0);
+            Grid.SetRowSpan(testImage2, 3);
+
+            Button saveChangesButton = new Button
+            {
+                Content = "Save changes",
+                Margin = new Thickness(5),
+                FontSize = 12,
+                Padding = new Thickness(5),
+                IsEnabled = false
+            };
+            Grid.SetRow(saveChangesButton, 3);
+            Grid.SetColumn(saveChangesButton, 1);
+            nestedGrid.Children.Add(saveChangesButton);
+
+            Button removeButton = new Button
+            {
+                Content = "Remove",
+                Margin = new Thickness(5),
+                FontSize = 12,
+                Padding = new Thickness(5),
+                IsEnabled = false
+            };
+            Grid.SetRow(removeButton, 3);
+            Grid.SetColumn(removeButton, 2);
+            nestedGrid.Children.Add(removeButton);
+
+            Button addButton = new Button
+            {
+                Content = "Add new",
+                Margin = new Thickness(5),
+                FontSize = 12,
+                Padding = new Thickness(5),
+                IsEnabled = true
+            };
+            Grid.SetRow(addButton, 4);
+            Grid.SetColumn(addButton, 1);
+            nestedGrid.Children.Add(addButton);
+            Grid.SetColumnSpan(addButton, 2);
+
+            Button saveButton = new Button
+            {
+                Content = "Save to file",
+                Margin = new Thickness(90, 5, 150, 5),
+                FontSize = 12,
+                Padding = new Thickness(5),
+            };
+            Grid.SetRow(saveButton, 1);
+            Grid.SetColumn(saveButton, 2);
+            assortmentGrid.Children.Add(saveButton);
 
 
             return assortmentGrid;
