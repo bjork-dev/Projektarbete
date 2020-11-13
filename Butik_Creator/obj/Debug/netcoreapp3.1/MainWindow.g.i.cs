fd5480431680cfb3d95ed36fea8d681638a3b2ca
Svetlana Ernyleva <70198472+SvetlanaErn@users.noreply.github.com>
2020-11-13 11:37:58 +0100

    Remove ButikTest1

--- Butik_Creator/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ Butik_Creator/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace Butik_Creator {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Butik_Creator;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Butik_Creator;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
